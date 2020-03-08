9e8549ebf8c7d5ef475d2feaf9423c6d68519b08
seimon-m <simimueller@hotmail.com>
2020-03-08 22:23:04 +0100

    Pong 2D V1

--- webgl_pong_v1/pong.js
+++ webgl_pong_v1/pong.js
@@ -1,18 +1,20 @@
-//
-// Computer Graphics
-//
-// WebGL Exercises
-//
+/*
+* Computer Graphics
+*
+* PONG by Simon Müller
+*/
+
 
 // Register function to call after document has loaded
 window.onload = startup;
 
 // the gl object is saved globally
-var gl;
-var collisionP1 = false;
+let gl;
+let globalAngle = 0;
+
 
 // we keep all local parameters for the program in a single object with the name ctx (for context)
-var ctx = {
+let ctx = {
     shaderProgram: -1,
     aVertexPositionId: -1,
     uColorId: -1,
@@ -25,12 +27,17 @@ var ctx = {
  */
 function startup() {
     "use strict";
-    var canvas = document.getElementById("myCanvas");
+    let canvas = document.getElementById("myCanvas");
+    const height = canvas.height;
+    const width = canvas.width;
     gl = createGLContext(canvas);
     initGL();
+
     draw();
     window.requestAnimationFrame(drawAnimated);
 }
+
+
 
 /**
  * InitGL should contain the functionality that needs to be executed only once
@@ -60,8 +67,8 @@ function setupAttributesAndUniforms() {
 }
 
 
-// we keep all the parameters for drawing aspecific object together
-var rectangleObject = {
+// we keep all the parameters for drawing a specific object together
+let rectangleObject = {
     coordinateBuffer: -1,
     colorBuffer: -1
 };
@@ -72,7 +79,7 @@ var rectangleObject = {
 function setupBuffers() {
     "use strict";
     rectangleObject.coordinateBuffer = gl.createBuffer();
-    var vertices = [
+    let vertices = [
         -0.5, -0.5,
         -0.5, 0.5,
         0.5, 0.5,
@@ -83,41 +90,74 @@ function setupBuffers() {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
     rectangleObject.colorBuffer = gl.createBuffer();
-    var colors = [
+    let colors = [
         1, 0, 0.4, 1,
         0.1, 0.2, 0.3, 1,
         0.3, 0.4, 0.5, 1,
-        0.3, 1, 0.1, 1
+        1, 1, 0.1, 1
     ];
 
     gl.bindBuffer(gl.ARRAY_BUFFER, rectangleObject.colorBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
 }
 
+class Ball {
+    constructor() {
+        this.x = Math.floor(Math.random() * (200 - (-200)) ) + (-200);
+        this.y = Math.floor(Math.random() * (100 - (-100))) + (-100);
+        this.xSpeed = Math.floor(Math.random() * (5 - (2))) + (2);
+        this.ySpeed = this.xSpeed;
+        this.size = 15;
+    }
 
-var game = {
-    ball: {
-        posX: 200,
-        posY: -100,
-        velocityX: 1,
-        velocityY: 1,
-        size: 10
-    },
-    paddle1: {
-        y: 0,
-        width: 10,
-        height: 60,
-        velocity: 1,
-        lives: 3
-    },
-    paddle2: {
-        y: 0,
-        width: 10,
-        height: 60,
-        velocity: 1,
-        lives: 3
+    move() {
+        this.x = this.x + this.xSpeed;
+        this.y = this.y + this.ySpeed;
+    }
+
+    bounce() {
+        if (this.y >= 300 || this.y <= -300) {
+            this.ySpeed = this.ySpeed * -1;
+        }
+        if (this.x <= -400) {
+            paddle2.lives--;
+            document.location.reload();
+        }
+        if (this.x >= 400) {
+            paddle1.lives--;
+            document.location.reload();
+        }
+    }
+
+    paddleCollision() {
+        if ( (ball.y >= paddle1.y-30) &&
+            (ball.y <= paddle1.y+30) &&
+            (ball.x >= 360)) {
+            this.xSpeed = this.xSpeed * -1;
+        }
+        if ( (ball.y >= paddle2.y-30) &&
+            (ball.y <= paddle2.y+30) &&
+            (ball.x <= -360)) {
+            this.xSpeed = this.xSpeed * -1;
+        }
     }
 }
+
+class Paddle {
+    constructor(lives = 3) {
+        this.y = 0;
+        this.width = 10;
+        this.height = 60;
+        this.velocity = 30;
+        this.lives = 3;
+    }
+
+}
+
+let ball = new Ball();
+let paddle1 = new Paddle();
+let paddle2 = new Paddle();
+
 
 /**
  * Draw the scene.
@@ -137,13 +177,14 @@ function draw() {
     gl.vertexAttribPointer(ctx.aColorId, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(ctx.aColorId);
 
-    //gl.uniform4f(ctx.uColorId , 1.0, 2.0, 0.0, 1.0);
-
     // Set up the world coordinates
-    var projectionMat = mat3.create();
+    let projectionMat = mat3.create();
     mat3.fromScaling(projectionMat, [2.0 / gl.drawingBufferWidth, 2.0 / gl.drawingBufferHeight]);
     gl.uniformMatrix3fv(ctx.uProjectionMatId, false, projectionMat);
 
+    ball.bounce();
+    ball.move();
+    ball.paddleCollision();
     drawPaddles();
     drawLine();
     drawBall();
@@ -151,14 +192,14 @@ function draw() {
 
 function drawPaddles() {
     var modelMat = mat3.create();
-    mat3.fromTranslation(modelMat, [370, game.paddle1.y]);
-    mat3.scale(modelMat, modelMat, [game.paddle1.width, game.paddle1.height]);
+    mat3.fromTranslation(modelMat, [370, paddle1.y]);
+    mat3.scale(modelMat, modelMat, [paddle1.width, paddle1.height]);
     gl.uniformMatrix3fv(ctx.uModelMatId, false, modelMat);
     gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
 
     var modelMat = mat3.create();
-    mat3.fromTranslation(modelMat, [-370, game.paddle2.y]);
-    mat3.scale(modelMat, modelMat, [game.paddle2.width, game.paddle2.height]);
+    mat3.fromTranslation(modelMat, [-370, paddle2.y]);
+    mat3.scale(modelMat, modelMat, [paddle2.width, paddle2.height]);
     gl.uniformMatrix3fv(ctx.uModelMatId, false, modelMat);
     gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
 }
@@ -172,32 +213,20 @@ function drawLine() {
 
 function drawBall() {
     var modelMat = mat3.create();
-    mat3.fromTranslation(modelMat, [game.ball.posX, game.ball.posY]);
-    mat3.scale(modelMat, modelMat, [game.ball.size, game.ball.size]);
+    mat3.fromTranslation(modelMat, [ball.x, ball.y]);
+    mat3.scale(modelMat, modelMat, [ball.size, ball.size]);
+    mat3.rotate(modelMat, modelMat, globalAngle);
     gl.uniformMatrix3fv(ctx.uModelMatId, false, modelMat);
-
-    if (game.ball.posX >= 370 && game.ball.posY >= game.paddle1.y-30 && game.ball.posY <= game.paddle1.y+30) {
-        collisionP1 = true;
-    }
-    if (game.ball.posY <= gl.drawingBufferHeight && game.ball.posY >= -200) {
-
-    }
-    if (collisionP1) {
-        game.ball.posX -= game.ball.velocityX;
-        game.ball.posY += game.ball.velocityY;
-    } else {
-        game.ball.posX += game.ball.velocityX;
-        game.ball.posY += game.ball.velocityY;
-    }
-
-
-
     gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
 }
 
-function drawAnimated(timeStamp) {
-    // calculate time since last call
-    // move or change objects
+function drawAnimated() {
+    if (globalAngle >= 2 * Math.PI) {
+        globalAngle = 0;
+    } else {
+        globalAngle +=  0.05;
+    }
+
     draw();
     // request the next frame
     window.requestAnimationFrame(drawAnimated);
@@ -209,26 +238,29 @@ document.addEventListener('keydown', (event) => {
     // paddle 1
     if (keyName === 'ArrowUp') {
         //alert(`Key pressed ${keyName}`);
-        if (game.paddle1.y <= 50) {
-            game.paddle1.y += 20;
-        } else {
-            game.paddle1.y = 60;
+        if (paddle1.y <= 260) {
+            paddle1.y += paddle1.velocity;
         }
     }
     if (keyName === 'ArrowDown') {
-        //alert(`Key pressed ${keyName}`);
-        game.paddle1.y -= 20;
+        if (paddle1.y >= -260) {
+            paddle1.y -= paddle1.velocity;
+        }
     }
 
     // paddle 2
     if (keyName === 'w') {
-        //alert(`Key pressed ${keyName}`);
-        game.paddle2.y += 20;
+        if (paddle2.y <= 260) {
+            paddle2.y += paddle2.velocity;
+        }
     }
     if (keyName === 's') {
-        //alert(`Key pressed ${keyName}`);
-        game.paddle2.y -= 20;
+        if (paddle2.y >= -260) {
+            paddle2.y -= paddle2.velocity;
+        }
     }
 
 }, false);
+
+
 
